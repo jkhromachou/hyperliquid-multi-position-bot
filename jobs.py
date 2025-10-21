67fceb46edded15c305aa8caf80616fe5b8b3b7f
jkhromachou <62083932+jkhromachou@users.noreply.github.com>
2025-10-21 16:54:01 -0500

    Create jobs.py

---
+++ jobs.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+class PositionJob:
+    def __init__(self, asset, side, entry_price, rule):
+        self.asset = asset
+        self.side = side
+        self.entry_price = entry_price
+        self.rule = rule
+        self.triggered = False
+        self.closed = False
+        self.open_size = None
+        self.entry_time = datetime.utcnow()
+
+    def __repr__(self):
+        return f"<Job {self.asset} {self.side} rule={self.rule.__name__}>"
